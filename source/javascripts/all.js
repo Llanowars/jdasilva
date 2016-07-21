597cd147ad8d3d2c187acd8aa275dc18f5076a56
Julien Da Silva <julien.da.silva@hotmail.com>
2016-07-21 15:48:16 +0200

    loading jquery

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,3 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
-// $(function() {
-//     $(".fa fa-star").focus(function(){
-//         $(this).val({padding:"10px"});
-//     })
-// });
-
-$(".fa fa-star").hover(function(event) {
-  $(this).toggle({padding: "10px"})
-});
