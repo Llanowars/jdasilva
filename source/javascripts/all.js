29a9a910428d81e0e8fdcb48e215a08fdbc61ceb
Julien Da Silva <julien.da.silva@hotmail.com>
2016-07-31 22:29:52 +0200

    adding FadeIn to navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,6 +2,7 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
+// Smooth scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -16,3 +17,25 @@ $(function() {
     }
   });
 });
+
+// Hide navbar when 200px are scrolled
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 200) { //use `this`, not `document`
+//         $('.navbar').css({
+//             'display': 'none'
+//         });
+//     }
+// });
+
+// toggle fadeIn/Out
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 900)
+     {
+        $('.navbar').fadeIn();
+        $('.navbar').removeClass("hidden");
+     }
+    else
+     {
+      $('.navbar').fadeOut();
+     }
+ });
