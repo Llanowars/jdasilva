0440f2883dee41c4fc0174635a098881860b82bc
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-01 20:29:57 +0200

    fixing navbar fadeIn for different screen height sizes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,40 +2,47 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-// Smooth scroll
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+$(document).ready(function() {
+ // the DOM is now ready, you can traverse it,
+ // and register events with on().
+
+  // Smooth scroll
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
+
+  // Hide navbar when 200px are scrolled
+  // $(window).scroll(function() {
+  //     if ($(this).scrollTop() > 200) { //use `this`, not `document`
+  //         $('.navbar').css({
+  //             'display': 'none'
+  //         });
+  //     }
+  // });
+
+  // toggle fadeIn/Out
+  var vH = $(window).height();
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > vH - 5)
+       {
+          $('.navbar').fadeIn();
+          $('.navbar').removeClass("hidden");
+       }
+      else
+       {
+        $('.navbar').fadeOut();
+       }
+   });
+
 });
-
-// Hide navbar when 200px are scrolled
-// $(window).scroll(function() {
-//     if ($(this).scrollTop() > 200) { //use `this`, not `document`
-//         $('.navbar').css({
-//             'display': 'none'
-//         });
-//     }
-// });
-
-// toggle fadeIn/Out
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 900)
-     {
-        $('.navbar').fadeIn();
-        $('.navbar').removeClass("hidden");
-     }
-    else
-     {
-      $('.navbar').fadeOut();
-     }
- });
