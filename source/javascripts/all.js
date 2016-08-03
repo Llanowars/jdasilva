0d84979e30fafc3a016c0f726b36081aee2effbf
Julien Da Silva <julien.da.silva@hotmail.com>
2016-08-03 11:00:05 +0200

    attempt of active states on sticky navbar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,8 +3,6 @@
 //= require_tree .
 
 $(document).ready(function() {
- // the DOM is now ready, you can traverse it,
- // and register events with on().
 
   // Smooth scroll
   $(function() {
@@ -22,16 +20,7 @@ $(document).ready(function() {
     });
   });
 
-  // Hide navbar when 200px are scrolled
-  // $(window).scroll(function() {
-  //     if ($(this).scrollTop() > 200) { //use `this`, not `document`
-  //         $('.navbar').css({
-  //             'display': 'none'
-  //         });
-  //     }
-  // });
-
-  // toggle fadeIn/Out
+  // navbar fadeIn/Out
   var vH = $(window).height();
   $(window).scroll(function() {
       if ($(this).scrollTop() > vH - 5)
@@ -44,5 +33,50 @@ $(document).ready(function() {
         $('.navbar').fadeOut();
        }
    });
+
+  // navbar addClass/removeClass active 
+  $(function() {
+      $('.navbar li a').click(function() {
+         $('.navbar li a').removeClass();
+         $(this).addClass('active-nav');
+      });
+   });
+
+  //Activate state off links in navbar
+  // var sections = $('.section')
+  // , nav = $('navbar');
+
+  // $(window).on('scroll', function () {
+  //   var cur_pos = $(this).scrollTop();
+    
+  //   sections.each(function() {
+  //     var top = $(this).offset().top,
+  //         bottom = top + $(this).outerHeight();
+      
+  //     if (cur_pos >= top && cur_pos <= bottom) {
+  //       nav.find('a').removeClass('active-nav');
+  //       sections.removeClass('active-nav');
+        
+  //       $(this).addClass('active-nav');
+  //       nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active-nav');
+  //     }
+  //   });
+  // });
+  
+  function onScroll(event){
+      var scrollPos = $(document).scrollTop();
+      $('.navbar a').each(function () {
+          var currLink = $(this);
+          var refElement = $(currLink.attr("href"));
+          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+              $('.navbar ul li a').removeClass("active-nav");
+              currLink.addClass("active-nav");
+          }
+          else{
+              currLink.removeClass("active-nav");
+          }
+      });
+    }
+
 
 });
