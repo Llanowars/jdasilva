2020c8dc0969ae123658c31c4041d0946a76aff0
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-07 14:06:30 +0200

    smallest banner

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -20,19 +20,31 @@ $(document).ready(function() {
     });
   });
 
-  // navbar fadeIn/Out
-  var vH = $(window).height();
+  // OLD navbar fadeIn/Out
+  // var vH = $(window).height();
+  // $(window).scroll(function() {
+  //     if ($(this).scrollTop() > vH - 5)
+  //      {
+  //         $('.navbar').fadeIn();
+  //         $('.navbar').removeClass("hidden");
+  //      }
+  //     else
+  //      {
+  //       $('.navbar').fadeOut();
+  //      }
+  //  });
+
+  // Navbar fadeIn/Out
   $(window).scroll(function() {
-      if ($(this).scrollTop() > vH - 5)
-       {
-          $('.navbar').fadeIn();
-          $('.navbar').removeClass("hidden");
-       }
-      else
-       {
+    if ($(this).scrollTop() + 2 >= $('.profile').offset().top) {
+      $('.navbar').fadeIn();
+      $('.navbar').removeClass("hidden");
+      }
+    else  
+      {
         $('.navbar').fadeOut();
-       }
-   });
+      }
+  });
 
   // navbar addClass/removeClass on click
   $(function() {
@@ -44,12 +56,11 @@ $(document).ready(function() {
   
   // navbar addClass/removeClass on scroll
   $(window).scroll(function() {
-
-    if ($(this).scrollTop() >= $('.profile').offset().top) {
+    if ($(this).scrollTop() + 2 >= $('.profile').offset().top) {
       $('.navbar li a').removeClass('active-nav');
       $('.navbar li a:eq(0)').addClass('active-nav');
     }
-    if ($(this).scrollTop() >= $('.abilities').offset().top) {
+    if ($(this).scrollTop() + 2 >= $('.abilities').offset().top) {
       $('.navbar a').removeClass('active-nav');
       $('.navbar a:eq(1)').addClass('active-nav');
     }
