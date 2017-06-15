a92cb0ba461326e6305b07c4e15a2f9c1d16d1b1
julien.da.silva@hotmail.com <julien.da.silva@hotmail.com>
2017-06-15 18:12:03 +0200

    Automated commit at 2017-06-15 16:12:03 UTC by middleman-deploy 1.0.0

---
+++ javascripts/navbar.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  // Navbar fadeIn/Out
+  $(window).scroll(function() {
+    if ($(this).scrollTop() + 2 >= $('.profile').offset().top) {
+      $('.navbar').fadeIn();
+      $('.navbar').removeClass("hidden");
+      }
+    else  
+      {
+        $('.navbar').fadeOut();
+      }
+  });
+
+  // OLD navbar fadeIn/Out
+  // var vH = $(window).height();
+  // $(window).scroll(function() {
+  //     if ($(this).scrollTop() > vH - 5)
+  //      {
+  //         $('.navbar').fadeIn();
+  //         $('.navbar').removeClass("hidden");
+  //      }
+  //     else
+  //      {
+  //       $('.navbar').fadeOut();
+  //      }
+  //  });
+
+
+  // navbar addClass/removeClass on click
+  // $(function() {
+  //     $('.navbar li a').click(function() {
+  //        $('.navbar li a').removeClass("active-nav");
+  //        $(this).addClass('active-nav');
+  //     });
+  //  });
+  
+  // navbar addClass/removeClass on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() + 2 >= $('.profile').offset().top) {
+      $('.navbar li a').removeClass('active-nav');
+      $('.navbar li a:eq(0)').addClass('active-nav');
+    }
+    if ($(this).scrollTop() + 2 >= $('.abilities').offset().top) {
+      $('.navbar a').removeClass('active-nav');
+      $('.navbar a:eq(1)').addClass('active-nav');
+    }
+  });
+ 
+});
